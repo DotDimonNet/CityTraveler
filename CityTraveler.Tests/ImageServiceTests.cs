293011c2c3a0fdfaef4c1c348b34d23a7b4a1e0d
njqdj <sergejminaev18@gmail.com>
2021-10-28 21:18:55 +0300

    Added ImageServiceTest file

---
+++ CityTraveler.Tests/ImageServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using CityTraveler.Domain.Entities;
+using CityTraveler.Domain.Errors;
+using CityTraveler.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityTraveler.Tests
+{
+    class ImageServiceTests
+    {
+        [SetUp]
+        public async Task Setup()
+        {
+            await ArrangeTests.SetupDbContext();
+        }
+    }
+}
