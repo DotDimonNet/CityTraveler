2c4077f15661bea9113efc935eb05240d2bafff6
cat-ears <75044687+cat-ears@users.noreply.github.com>
2021-10-31 17:35:59 +0200

    Trip controller

---
+++ CityTraveler.Domain/Errors/TripControllerException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityTraveler.Domain.Errors
+{
+    [Serializable]
+    public class TripControllerException:Exception
+    {
+        public TripControllerException() : base() { }
+        public TripControllerException(string message) : base(message) { }
+        public TripControllerException(string message, Exception innerException) : base(message, innerException) { }
+        public TripControllerException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
