fe10e000d8ab6d773c9762e13c39e33d78f53a04
Kate <kateryna.kondratiuk@ukma.edu.ua>
2021-11-03 12:46:03 +0200

    DTOs for social media service

--- CityTraveler.Domain/Entities/ReviewModel.cs
+++ CityTraveler.Domain/Entities/ReviewModel.cs
@@ -7,7 +7,7 @@ namespace CityTraveler.Domain.Entities
 {
     public class ReviewModel : Entity, IDescribable
     {
-        public virtual Guid RatingId { get; set; }
+        public virtual Guid? RatingId { get; set; }
         public virtual RatingModel Rating { get; set; }
         public virtual Guid UserId { get; set; }
         public virtual ApplicationUserModel User { get; set; }
@@ -18,7 +18,7 @@ namespace CityTraveler.Domain.Entities
     }
     public class EntertainmentReviewModel : ReviewModel 
     {
-        public virtual Guid EntertaimentId { get; set; }
+        public virtual Guid EntertainmentId { get; set; }
         public virtual EntertaimentModel Entertaiment { get; set; }
     }
     public class TripReviewModel : ReviewModel 
